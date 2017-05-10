25116b29ed0eeb358d762ddfcfdd8a7e6650ad2a
Paul <pm-simon@outlook.de>
2017-05-11 00:08:09 +0200

    testFürGit

--- Organizer/app/src/main/java/com/np_app/paul/organizer/MainActivity.java
+++ Organizer/app/src/main/java/com/np_app/paul/organizer/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+//TestFürGit
     Button kamera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
